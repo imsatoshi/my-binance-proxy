8a47327e3efddbed63291f898a5e8b417226847b
jwzhang886 <aaronzjw@icloud.com>
2025-10-20 18:03:39 +0800

    feat: update

--- src/routes/proxy.js
+++ src/routes/proxy.js
@@ -10,11 +10,13 @@ const logger = require('../utils/logger');
 router.all('*', async (req, res, next) => {
   try {
     const method = req.method;
-    const endpoint = req.path;
+    // Prepend /api to the path since Express strips it from req.path
+    const endpoint = '/api' + req.path;
     const params = { ...req.query, ...req.body };
 
     logger.info(`Proxying ${method} request`, {
       endpoint,
+      originalPath: req.path,
       ip: req.ip,
       userAgent: req.get('user-agent')
     });
